84f961ba25e3f1ce83bdcd00b1a043f1bcfc1f66
KYs <amitkys59@gmail.com>
2024-09-30 01:37:54 +0530

    added a text

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count in test {count}
+          count in test with script {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
